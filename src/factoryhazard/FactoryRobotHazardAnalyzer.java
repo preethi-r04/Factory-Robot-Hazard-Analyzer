b3e3ab17a2beb55b773709ac398e6a71bcb13357
preethi-r04 <preethiliki2004@gmail.com>
2026-01-29 14:47:58 +0530

    [Preethi]: Added: UC1: Display static Factory Robot Hazard Analyzer message

---
+++ src/factoryhazard/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,6 @@
+package factoryhazard;
+public class FactoryRobotHazardAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
