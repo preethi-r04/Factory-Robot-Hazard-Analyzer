8642c35870878138f68e6528892173327aaac9bb
preethi-r04 <preethiliki2004@gmail.com>
2026-01-29 15:48:17 +0530

    [Preethi]: Added: UC5: Added input validation with custom exception handling

---
+++ src/factoryhazard/InvalidHazardInputException.java
@@ -0,0 +1,10 @@
+package factoryhazard;
+
+// UC4: Custom exception
+public class InvalidHazardInputException extends Exception {
+
+    public InvalidHazardInputException(String message) {
+        super(message);
+    }
+}
+
